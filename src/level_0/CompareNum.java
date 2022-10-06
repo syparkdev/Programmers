5c93b693634050662f50b08908f4854a95c08125
Seiyon Park <seiyonpdev@gmail.com>
2022-10-07 04:29:18 +0900

    Level 0_CompareNum
    First Commit!

---
+++ src/level_0/CompareNum.java
@@ -0,0 +1,10 @@
+package level_0;
+
+public class CompareNum {
+	
+	 public int solution(int num1, int num2) {
+	        int answer = (num1 == num2) ? 1 : -1;
+	        
+	        return answer;
+	 }
+}
