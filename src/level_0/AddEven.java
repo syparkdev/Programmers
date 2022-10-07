a6919a6ac1e45ac3c04283391ae62cfb8c6c389b
Seiyon Park <seiyonpdev@gmail.com>
2022-10-07 13:53:27 +0900

    feat:Added AddEven method

---
+++ src/level_0/AddEven.java
@@ -0,0 +1,16 @@
+package level_0;
+
+//정수 n을 입력받아 0부터 n까지 짝수의 합을 구하는 메소드
+
+public class AddEven {
+	
+	public int solution(int n) {
+        int answer = 0;
+
+		for(int i = 2;i <= n;i+=2) {
+			answer += i;
+		}
+		
+        return answer;
+    }
+}
