39ee008f49c88ce793b211f8a3c1fc8d685b6499
Seiyon Park <seiyonpdev@gmail.com>
2022-10-07 14:35:08 +0900

    feat:Added ArrayDouble method

---
+++ src/level_0/ArrayDouble.java
@@ -0,0 +1,18 @@
+package level_0;
+
+public class ArrayDouble {
+	
+	 public int[] solution(int[] numbers) {
+		 
+	        int[] answer = new int[numbers.length];
+        	int index = 0;
+
+	        for (int i : numbers) {
+
+	        	answer[index] = i * 2;
+	        	index++;
+	        	}
+
+	        return answer;
+	    }
+}
