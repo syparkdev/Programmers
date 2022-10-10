01677f019eff81ccb15f3320008589e8049e1d48
Seiyon Park <seiyonpdev@gmail.com>
2022-10-10 13:14:36 +0900

    feat:+SumJaritsu method, docs:style:in SumDivisor method
    SumJaritsu 메소드를 추가했다.
    SumDivisor 메소드에 주석을 추가하고 줄바꿈을 정리했다.

--- src/level_1/SumDivisor.java
+++ src/level_1/SumDivisor.java
@@ -1,5 +1,7 @@
 package level_1;
 
+//정수 n을 받아 n의 약수를 전부 더하는 메소드
+
 public class SumDivisor {
 
 	public static int SumDivisor(int n) {
@@ -7,6 +9,7 @@ public class SumDivisor {
 		int sum = 0;
 
 		for (int i = 1; i <= n; i++) {
+			
 			if (n % i == 0) sum += i;
 		}
 
---
+++ src/level_1/SumJaritsu.java
@@ -0,0 +1,19 @@
+package level_1;
+
+//자연수 n을 받아 n의 자릿수를 전부 더하는 메소드
+
+public class SumJaritsu {
+	
+	public static int SumJaritsu (int n) {
+		
+		int sum = 0;
+		
+		for (int i = n; i > 0; i /= 10) {
+			
+			sum += i % 10;
+		}
+		
+		return sum;
+	}
+	
+}
