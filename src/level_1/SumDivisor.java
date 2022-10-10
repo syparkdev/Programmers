4369490962d0eddb645aecf53257dabe28fff8dc
Seiyon Park <seiyonpdev@gmail.com>
2022-10-10 12:11:09 +0900

    feat:+SumDivisor method

---
+++ src/level_1/SumDivisor.java
@@ -0,0 +1,16 @@
+package level_1;
+
+public class SumDivisor {
+
+	public static int SumDivisor(int n) {
+
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) sum += i;
+		}
+
+		return sum;
+	}
+	
+}
