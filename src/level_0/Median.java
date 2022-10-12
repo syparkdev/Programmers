f9c748ce052fb53fa862b8cabb78ab9bf9a10dd9
Seiyon Park <seiyonpdev@gmail.com>
2022-10-12 19:27:58 +0900

    feat: ADD method Median

---
+++ src/level_0/Median.java
@@ -0,0 +1,19 @@
+package level_0;
+
+import java.util.Arrays;
+
+//배열의 중앙값을 반환하는 메소드
+
+public class Median {
+	
+	public int median (int array[]) {
+		
+		int midIndex = array.length / 2 + 1;
+		
+		Arrays.sort(array);
+		
+		return array[midIndex];
+	}
+
+
+}
